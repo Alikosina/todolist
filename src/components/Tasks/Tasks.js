ac4178dfc7aa5f84cb4feb5454549f48fbf3e338
Admin <admin@MacBook-Pro-Admin.local>
2019-08-19 13:31:47 +0300

    Использовал memo

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,16 +1,15 @@
-import * as React from "react";
+
+import React , { memo } from "react";
 import "./Tasks.scss";
 
 
-export default class Tasks extends React.PureComponent{
-    render(){
-        return(
-            <div className="tasks-item">
-                <div> 
-                    <div className="tasks-item__title"> {this.props.title} </div>
-                    {this.props.children}
-                 </div>
-            </div> 
-        )
-    }
-}
+
+const Tasks = (props) =>(
+        <div className="tasks-item">
+                 <div> 
+                     <div className="tasks-item__title"> {props.title} </div>
+                     {props.children}
+                  </div>
+             </div> 
+)
+export default memo(Tasks)
