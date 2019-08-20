5681be056d49d67900a52e6aeeffc5a3e71ca588
Admin <admin@MacBook-Pro-Admin.local>
2019-08-20 13:52:11 +0300

    Изменил классы  на фунциональные компоненты

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,34 +1,21 @@
-import * as React from "react";
+import  React , {useState , memo} from "react";
 import { Button } from 'semantic-ui-react'
 
-
-
-export default class Form extends React.PureComponent{
-    constructor(props){
-        super(props);
-        this.nameChange = this.nameChange.bind(this);
-        this.onClick = this.onClick.bind(this);
-        this.state = { value : ''}
+const Form = (props) =>{
+    const { addState } = props 
+    const [value , setValue] = useState('')
+    const nameChange = (e) => {
+        setValue(e.target.value)
+     }
+     const onClick = () => {
+        addState(value);
+       setValue('')
     }
-
-     nameChange(e) {
-        this.setState({
-            value : e.target.value,  
-        })
-    }
-
-    onClick() {
-        this.props.addState(this.state.value);
-        this.setState({
-            value: ''
-        })
-    }
-    render(){  
-        return(
-            <div className='add-task'>
-                <input className='inp' placeholder='Что нужно сделать' value={this.state.value} onChange={this.nameChange}/>
-                <div> <Button primary onClick={this.onClick}>Click</Button> </div>
-            </div>
-        )
-    }
+    return(
+        <div className='add-task'>
+            <input className='inp' placeholder='Что нужно сделать' value={value} onChange={nameChange}/>
+            <div> <Button primary onClick={onClick}>Click</Button> </div>
+        </div>
+    )
 }
+export default memo(Form)
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,41 +1,37 @@
-import * as React from "react";
+import  React , { memo , useState } from "react";
 import "./Todo.scss"
-// import { Button } from 'react-toolbox/lib/button';
-import { Button } from 'semantic-ui-react'
 import classNames from 'classnames';
 import Tasks from '../Tasks/Tasks';
 import Form from '../Form/Form';
 
-export default class Todo extends  React.Component{
-    constructor(props){
-        super(props);
-        this.add = this.add.bind(this);
-        this.deleteId = this.deleteId.bind(this);
-        this.createTask = this.createTask.bind(this);
-        this.done = this.done.bind(this);
-        this.state = { value : '' ,  tasks : []}
-    }
-    add (value ) {
-        const currentTasks = this.state.tasks.map(el => el)
-        const newTask = this.createTask(value)
+
+const Todo = () =>{
+    const [tasks , setTasks] = useState([]);
+
+    const createTask = (value) => {
+        return  {
+                 id : Math.random(),
+                 done : false ,
+                 text : value
+             }
+     }
+
+     const add = (value) => {
+        const currentTasks = tasks.map(el => el)
+        const newTask = createTask(value)
         currentTasks.push(newTask)
-        this.setState({
-            tasks: currentTasks
-        })
+        setTasks(currentTasks)
     }
 
-    deleteId (id) {
-        const tasks = this.state.tasks;
+    const deleteId = (id) => {
+        
         const newTask = tasks.filter(function(el){
             return el.id != id
         })
-        this.setState({
-            tasks : newTask
-        })
-    }
+        setTasks(newTask)
+    }    
 
-    done (id) {
-        const tasks = this.state.tasks;
+    const done = (id) => {
         const doned = tasks.map((el)=>{
             if (el.id === id){
                 el.done = true
@@ -43,41 +39,31 @@ export default class Todo extends  React.Component{
             }
             return el
         })
-        this.setState({ 
-            tasks : doned 
-        })  
+        setTasks(doned) 
     }
 
-    createTask(value) {
-       return  {
-                id : Math.random(),
-                done : false ,
-                text : value
-            }
-        
-    }
-    
-    render(){
-        const tasks = this.state.tasks;   
-        const list = tasks.map((task) => {
-             return (<div className="list"> <div  className={classNames( 'list-item' ,{ 'list__done': task.done })}> {task.text} </div> <span className="list-item list-item_delete" onClick={() => this.deleteId(task.id)}> Удалить задачу </span> <span className="list-item" onClick={() => this.done(task.id)}> Выполнена </span> </div> );
-          });
-          const listDoned = tasks.map((task) => {
-              if (task.done){
-                  return (
-                      <div className="list__doned">
-                          {task.text}
-                      </div>
-                  )
-              }
-          })
-      
-        return(<div className='block'>
-            <Form addState={this.add} changeName={this.nameChange}/>
-            <div className="tasks">
-                <Tasks title="Запланированные задачи"> {list} </Tasks>
-                <Tasks title="Выполненные задачи"> {listDoned} </Tasks>
-            </div>     
-        </div>)
-    }
+    const list = tasks.map((task) => {
+        return (<div className="list"> <div  className={classNames( 'list-item' ,{ 'list__done': task.done })}> {task.text} </div> <span className="list-item list-item_delete" onClick={() => deleteId(task.id)}> Удалить задачу </span> <span className="list-item" onClick={() => done(task.id)}> Выполнена </span> </div> );
+     });
+     const listDoned = tasks.map((task) => {
+         if (task.done){
+             return (
+                 <div className="list__doned">
+                     {task.text}
+                 </div>
+             )
+         }
+     })
+
+     return(<div className='block'>
+     <Form addState={add}/>
+     <div className="tasks">
+         <Tasks title="Запланированные задачи"> {list} </Tasks>
+         <Tasks title="Выполненные задачи"> {listDoned} </Tasks>
+     </div>     
+ </div>)
+
 }
+
+export default memo(Todo)
+
